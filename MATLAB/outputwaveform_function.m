8a7e2cf330aad31adf1410edad5b58e36f5184f5
Po-jui <108991334+Tosalbert@users.noreply.github.com>
2023-07-24 12:54:29 -0700

    fg waveform function

---
+++ MATLAB/outputwaveform_function.m
@@ -0,0 +1,9 @@
+outputwaveform(waveformGen, 'ramp', 1, 1);
+
+function [] = outputwaveform(waveformGen, waveform, offset, amplitude)
+
+fprintf(waveformGen, ':source:FUNCTION:shape %s', waveform);
+fprintf(waveformGen, 'SOURce:VOLTage:LEVel:IMMediate:OFFSET %d', offset);
+fprintf(waveformGen, 'SOURce:VOLTage:LEVel:IMMediate:amplitude %d', amplitude);
+
+end
